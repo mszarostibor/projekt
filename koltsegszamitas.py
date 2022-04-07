00ba3077c7a46fa7e42c39ef30c76103f76fdc9b
meszarostibor <71766087+mszarostibor@users.noreply.github.com>
2022-04-07 09:30:28 +0200

    Update koltsegszamitas.py

--- koltsegszamitas.py
+++ koltsegszamitas.py
@@ -1,7 +1,5 @@
-#Írjon Python programot, amelyben egy Irodaház teszthálózat tervezése alap-ján használt eszközeinek költségszámítását végzi el.
-
 def szamitas():
-
+    lista=[]
 
     router = int(input("Egy router ára: ")) * int(input("A routerek száma: "))
     laptop = int(input("Egy laptop ára: ")) * int(input("A laptopok száma: "))
@@ -10,8 +8,18 @@ def szamitas():
     mobil = int(input("Egy mobil ára: ")) * int(input("A mobilok száma: "))
     nyomtato = int(input("Egy nyomtató ára: ")) * int(input("A nyomtatók száma: "))
 
-    osszertek = router + laptop + szerver + pc + mobil + nyomtato
-    print(osszertek, "Ft")               
+    lista.append(router)
+    lista.append(laptop)
+    lista.append(szerver)
+    lista.append(pc)
+    lista.append(mobil)
+    lista.append(nyomtato)
+    print("A routerek ára:",lista[0], "Ft")
+    print("A laptopok ára:",lista[1], "Ft")
+    print("A szerverek ára:",lista[2], "Ft")
+    print("A pcék ára:",lista[3], "Ft")
+    print("A mobilok ára:",lista[4], "Ft")
+    print("A nyomtatók ára:",lista[5], "Ft")
 
 
 szamitas()
